36b375b4882a64a798fe86c7a51184918805c81a
ismailasylla <ismailasyllaa@gmail.com>
2022-10-19 15:34:07 +0400

    folders & files structure.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import React from "react";
+import {
+  Blog,
+  Features,
+  Footer,
+  Header,
+  Possibility,
+  WhatGPT3,
+} from "./containers";
+
+import { Cta, Brand, Navbar } from "./components";
 
 const App = () => {
-  return <div>GPT-3</div>;
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <Cta />
+      <Blog />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
